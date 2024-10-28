b0b3a1c29db937d7812fc3a5e9af68ef28b6b118
hyoseung2000 <hyoseung.lee2000@gmail.com>
2024-10-29 08:51:26 +0900

    chore : dataSource 설정

--- src/main/java/com/ssafy/sandbox/domain/todo/controller/TodoController.java
+++ src/main/java/com/ssafy/sandbox/domain/todo/controller/TodoController.java
@@ -1,7 +1,14 @@
 package com.ssafy.sandbox.domain.todo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
 }
