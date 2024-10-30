af0416aa127da99002cd7064dc5b7a13eb71ec2b
hyoseung2000 <hyoseung.lee2000@gmail.com>
2024-10-31 03:02:40 +0900

    fix: CrossOrigin 설정 추가

--- src/main/java/com/ssafy/sandbox/controller/TodoController.java
+++ src/main/java/com/ssafy/sandbox/controller/TodoController.java
@@ -6,6 +6,7 @@ import com.ssafy.sandbox.domain.todo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TodoController {
     private final TodoService todoService;
 
